77518a11d27b43530595ab63b9e2711ce517aa5d
PiotrDyrcz <128043786+PiotrDyrcz@users.noreply.github.com>
2023-10-06 15:35:23 +0200

    Update data i czas.php

--- data i czas.php
+++ data i czas.php
@@ -4,4 +4,16 @@
     $now = time();
     $week = 100*24*60*60;
     echo "<br> teraz: ".date("d.m.Y h:i:sa", $czas -$week );
+
+    // echo "Today is " . date("h:i:sa");
+
+    // $czas = mktime(11, 14, 54, 8, 12, 2014 );
+    // $now = time();
+    // $week = 7*24*60*60;
+    // echo "<br> teraz: ".date("d.m.Y h:i:sa", $now +$week );
+
+     // echo "Today is " . date("Y/m/d") . "<br>";
+     //    echo "Today is " . date("Y.m.d") . "<br>";
+     //    echo "Today is " . date("Y-m-d") . "<br>";
+     //    echo "Today is " . date("l") . "<br>";
 ?>
